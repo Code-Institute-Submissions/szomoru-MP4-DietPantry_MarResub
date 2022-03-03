b5813e6b3142b0556841ef172c9453d51e283aba
szomoru <gergely.vig1978@gmail.com>
2022-03-03 22:57:49 +0000

    checkout template and view added

--- dietpantry/settings.py
+++ dietpantry/settings.py
@@ -47,6 +47,9 @@ def jls_extract_def():
         'products',
         'bag',
         'checkout',
+
+        #Other
+        'crispy_forms',
     ]
 
 
@@ -64,6 +67,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'dietpantry.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -78,8 +83,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
