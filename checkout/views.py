968f0964e4ef2d235d4504a1550b309fb858c377
szomoru <gergely.vig1978@gmail.com>
2022-03-04 09:43:51 +0000

    Stripe payment system added to the checkout, stripe element javascript and checkout stylesheet created

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KZVCGIJ2p0kf1IzLNalTKkSWZfd4OK8IVd43MEpjUlnBYFtkqzKxICi76t2yjUTIjAONreBLhGbXsCqfA4bGnrd00pw2GuIvO',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
