8b5f8ba5ace9edb319d01f358b5e438b0fcef382
szomoru <gergely.vig1978@gmail.com>
2022-03-06 18:25:18 +0000

    payment overlay added

--- checkout/signals.py
+++ checkout/signals.py
@@ -3,20 +3,17 @@ from django.dispatch import receiver
 
 from .models import OrderLineItem
 
-
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
-    This method update order total on LineItem
-    Update/Create
+    Update order total on lineitem update/create
     """
     instance.order.update_total()
-
 
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
     """
-    This method update order total on LineItem
-    Delete
+    Update order total on lineitem delete
     """
     instance.order.update_total()
+    
