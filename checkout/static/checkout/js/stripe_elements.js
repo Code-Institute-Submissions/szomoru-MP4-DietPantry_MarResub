4e500ec35de4c9e1ee7b04e9880cc680e9e47020
szomoru <gergely.vig1978@gmail.com>
2022-03-06 17:16:47 +0000

    checlout success template added

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -3,6 +3,7 @@ Core logics source: https://stripe.com/docs/payments/accept-a-payment
 CSS source: https://stripe.com/docs/js
 */
 
+// Initiate Stripe Card element
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
@@ -25,7 +26,7 @@ var style = {
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-//Handle realtime validation errors on the card element
+// Provide the customer with validations error on the Stripe card element
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
@@ -41,8 +42,10 @@ card.addEventListener('change', function (event) {
     }
 });
 
-//Handle from submit
-
+/*
+Handle the Stripe card element form submission,
+core logic is taken from the Stripe documentation
+*/
 var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
@@ -52,8 +55,8 @@ form.addEventListener('submit', function(ev) {
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
 
-    var saveInfo = Boolean($('#id-save-info').attr('checked'));
-    // From using {% csrf_token %} in the form
+    var saveInfo = Boolean($('id-save-info').attr('checked'));
+    // CSRF value is obtained from the {% csrf_token %} in the form elements
     var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
     var postData = {
         'csrfmiddlewaretoken': csrfToken,
@@ -113,5 +116,5 @@ form.addEventListener('submit', function(ev) {
     }).fail(function () {
         // just reload the page, the error will be in django messages
         location.reload();
-    })
+    });
 });
