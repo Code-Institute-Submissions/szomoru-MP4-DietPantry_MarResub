f3caa10520e5301216695548096b924229d63e67
szomoru <gergely.vig1978@gmail.com>
2022-03-07 21:10:49 +0000

    Favourites views and models added

--- favourites/models.py
+++ favourites/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+
 
 # Create your models here.
+class Favourite(models.Model):
+    """
+    Model to show the items on the user favourites list
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, 
+                                    through="FavouriteItem", 
+                                    related_name='product_favourites')
+    
+    def __str__(self):
+        return f'Favourite({self.user})'
+
+
+class FavouriteItem(models.Model):
+    product = models.ForeignKey(Product,
+                                null=False,
+                                blank=False,
+                                on_delete=models.CASCADE)
+    favourite = models.ForeignKey(Favourite,
+                                null=False,
+                                blank=False,
+                                on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product.name
--- favourites/views.py
+++ favourites/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+from django.contrib import messages
+
+from products.models import Product
+from .models import Favourite
+
 
 # Create your views here.
+@login_required
+def favourite(request):
+    favourite = None
+    try:
+        favourite = Favourite.objects.get(user=request.user)
+    except Favourite.DoesNotExist:
+        pass
+
+    context = {
+        'favourite': favourite,
+    }
+
+    return render(request, 'favourites/favourites.html', context)
+
+
+@login_required
+def add_to_favourites(request, product_id):
+    """
+    Add products to the favourites for the registered user
+    This function require login
+    """
+    product = get_object_or_404(Product, pk=product_id)
+
+    # If favourite list is not exoiting it is going to create
+    favourite, _ = Favourite.objects.get_or_create(user=request.user)
+
+    # Adding product to the favourites
+    favourite.products.add(product)
+    messages.info(request, "A new product is added to your favourites")
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def remove_from_favourites(request, product_id):
+    """
+    Remove product from the favourites for the registered user
+    This function require login
+    """
+    favourite = Favourite.objects.get(user=request.user)
+    product = get_object_or_404(Product, pk=product_id)
+
+    # Removinging product from the favourites
+    favourite.products.remove(product)
+    messages.info(request, "A product has been removed from your favourites")
+
+    return redirect(request.META.get('HTTP_REFERER'))
