b52bde9ab3ae143f6713668d86abcf98c9727155
szomoru <gergely.vig1978@gmail.com>
2022-03-07 16:36:03 +0000

    Product management - product edit and delete function added to product and product detail page

--- products/views.py
+++ products/views.py
@@ -78,11 +78,11 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product Successfully Added!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Something went wrong! Please ensurethe form is valid.')
+            messages.error(request, 'Something went wrong! Please ensure the form is valid.')
     else:
         form = ProductForm()
 
@@ -118,3 +118,13 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """
+    Delete Product
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product Deleted!')
+    return redirect(reverse('products'))
